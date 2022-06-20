567e59ca55dd55a5ea03ce389913806c096ec20e
Guillermo Thourey <thoureyg@gmail.com>
2022-06-20 18:31:12 -0400

    Diseñando las cards del blog

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,39 @@ import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 import Link from "next/link";
 import Date from "../components/date";
+import Image from "next/image";
+import {
+  Box,
+  Heading,
+  Text,
+  Stack,
+  HStack,
+  Avatar,
+  Divider,
+} from "@chakra-ui/react";
+
+function Feature({ id, title, date }) {
+  return (
+    <Box p={5} shadow="md" borderWidth="2px" maxWidth={450}>
+      <Heading size="md" fontSize="50px">
+        <Link href={`/posts/${id}`}>{title}</Link>
+      </Heading>
+      <Divider py={5} />
+      <Stack mt={6} direction={"row"} spacing={4} align={"center"}>
+        <Avatar
+          src={"https://avatars0.githubusercontent.com/u/1164541?v=4"}
+          alt={"Author"}
+        />
+        <Stack direction={"column"} spacing={0} fontSize={"sm"}>
+          <Text fontWeight={600}>Achim Rolle</Text>
+          <Text color={"gray.500"}>
+            <Date dateString={date} />
+          </Text>
+        </Stack>
+      </Stack>
+    </Box>
+  );
+}
 
 export default function Home({ allPostsData }) {
   return (
@@ -23,13 +56,9 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              <Link href={`/posts/${id}`}>
-                <a>{title}</a>
-              </Link>
-              <br />
-              <small className={utilStyles.lightText}>
-                <Date dateString={date} />
-              </small>
+              <HStack spacing={8}>
+                <Feature id={id} title={title} date={date} />
+              </HStack>
             </li>
           ))}
         </ul>
