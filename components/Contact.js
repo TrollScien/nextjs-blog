abf2c4c04f39173acdd754d666974abb16eab532
Guillermo Thourey <thoureyg@gmail.com>
2022-07-14 17:17:20 -0400

    Agregado confetti al dar clic en enviar mensaje de contacto

--- components/Contact.js
+++ components/Contact.js
@@ -34,9 +34,63 @@ import {
 } from "react-icons/bs";
 import { MdOutlineEmail, MdLocationPin, MdPhone } from "react-icons/md";
 import Map from "./Map";
+import ReactCanvasConfetti from "react-canvas-confetti";
+
+const canvasStyles = {
+  position: "fixed",
+  pointerEvents: "none",
+  width: "100%",
+  height: "100%",
+  top: 0,
+  left: 0,
+};
+
+let animationInstance = null;
 
 export default function Contact() {
   const finalRef = React.useRef(null);
+
+  const makeShot = (particleRatio, opts) => {
+    animationInstance &&
+      animationInstance({
+        ...opts,
+        origin: { y: 0.7 },
+        particleCount: Math.floor(200 * particleRatio),
+      });
+  };
+
+  const fire = () => {
+    makeShot(0.25, {
+      spread: 26,
+      startVelocity: 55,
+    });
+
+    makeShot(0.2, {
+      spread: 60,
+    });
+
+    makeShot(0.35, {
+      spread: 100,
+      decay: 0.91,
+      scalar: 0.8,
+    });
+
+    makeShot(0.1, {
+      spread: 120,
+      startVelocity: 25,
+      decay: 0.92,
+      scalar: 1.2,
+    });
+
+    makeShot(0.1, {
+      spread: 120,
+      startVelocity: 45,
+    });
+  };
+
+  const getInstance = (instance) => {
+    animationInstance = instance;
+  };
 
   return (
     <Flex
@@ -215,6 +269,7 @@ export default function Contact() {
                       bg="#0D74FF"
                       color="white"
                       _hover={{}}
+                      onClick={fire}
                     >
                       Enviar Mensaje
                     </Button>
@@ -225,6 +280,7 @@ export default function Contact() {
           </VStack>
         </Box>
       </Box>
+      <ReactCanvasConfetti refConfetti={getInstance} style={canvasStyles} />
     </Flex>
   );
 }
